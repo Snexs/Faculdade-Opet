95ad6ce2d7999603d3ca3390768acc6268df2a80
João Gabriel Kasprik Arruda <jgkasprik@hotmail.com>
2020-12-05 05:19:15 -0300

    Update 1.2.5 - Correcao de varios bugs - Finalizado a tela de update - Finalizado a tela de exclude - Ajustado a tela de login - Criado metodo de validacao de login e senha - Ajustado o gerador de login - Alterado alguns detalhes de escrita no cmd - Alterado nome das regions - Alterado o nome de metodos e variaveis todos para PT-BR

--- Projeto-Integrador-2oSemestre/CtaSystem.c
+++ Projeto-Integrador-2oSemestre/CtaSystem.c
@@ -37,7 +37,7 @@ typedef struct RegisterAdmin{
     char senha[32];
 } RegisterAdmin_st;
 
-int opcao,contador = 0,contadorAdmin = 0,indiceUsuario = 1,indiceAdmin = 1,indiceCadastroUser = 0,indiceCadastroUserAdmin = 0;
+int opcao,indiceUsuario = 1,indiceAdmin = 1,indiceCadastroUsuario = 0,indiceCadastroAdmin = 0;
 
 Register_st cadastrar[MAX];
 Usuario_st usuario[MAX];
@@ -76,55 +76,77 @@ int login()
       break;
     }
 
-    char user_entry[32],senha_entry[32];
-	int tentativas = 0,i = 0;;
+    char *user_entry[60],*senha_entry[32];
+	int tentativas = 0;
     do {
         printf("Login: ");
-        scanf("%s", &user_entry[32]);
+        scanf("%s", &user_entry);
         printf("Senha: ");
-        scanf("%s", &senha_entry[32]);
+        scanf("%s", &senha_entry);
+        printf("----------------------------------\n");
         
-        //descobre o usuario na lista(struct)
-        //debugar o for e o login, pois nao esta funcionando
-        for (i = 0; i < MAX; i++){
-            printf("Dentro do for %d!\n",i);
-            if (!strcmp("", user_entry[32])){
-                printf("Nenhum usuário encontrado, abrindo cadastro!\n");
-                cadastroCliente();
-            }else{
-                if (!strcmp(user_entry[32],usuario[i].login)){
-                    printf("Dentro do for e do IF USER %d!\n",i);
-                    return i;
-                }else{
-                    if (!strcmp(user_entry[32],usuario[i].adminUser)){
-                        printf("Dentro do for do if e do IF ADMIN %d!\n",i);
-                        return i;
-                    }
-                }
-            }   
-        }
-  
-        //validacoes login
-        if (!strcmp(user_entry[32], usuario[i].login) && !strcmp(senha_entry[32], usuario[i].senha)) {
-            printf("Bem vindo %s\n", usuario[i].login);
-            return ListClient();
-        }else{
-            if (!strcmp(user_entry[32], usuario[i].adminUser) && !strcmp(senha_entry[32], usuario[i].senha)) {
-            printf("Bem vindo %s\n", usuario[i].adminUser);
+        int responseLogin = ValidarLogin(user_entry);
+        int responseSenha = ValidarSenha(senha_entry);
+
+        //Processo de login
+        if (!strcmp(user_entry, usuario[responseLogin].login) && !strcmp(senha_entry, usuario[responseSenha].senha)) {
+            printf("Bem vindo %s\n", usuario[responseLogin].login);
+            return dashboard();
+        }else if (!strcmp(user_entry, usuario[responseLogin].adminUser) && !strcmp(senha_entry, usuario[responseSenha].senha)){
+            printf("Bem vindo %s\n", usuario[responseLogin].adminUser);
             return dashboardAdmin();
-            } else {
-            printf("Usuario ou senha invalidos!\n");
-            tentativas++;
-            }
         }
+        //Fim processo de login
+
+        //Validar Login
+        
+        if (responseLogin == 0 || responseSenha == 0)
+        {
+            printf("Usuario ou Senha Invalidos!\n");
+            tentativas++;
+        }
+        //Fim validar login
     } while (tentativas < 3);
     printf("Numero maximo de tentativas alcancado!\n");
+    main();
+}
+
+#pragma endregion
+
+#pragma region Validacao Login
+
+int ValidarLogin(char *user_entry){
+int i;
+for (i = 1; i < MAX; i++){
+    if (!strcmp(user_entry,usuario[i].login))
+        return i;
+    if (!strcmp(user_entry,usuario[i].adminUser))
+        return i;
+}
+if (i >= MAX)
     return 0;
 }
 
 #pragma endregion
 
-#pragma region Funcao Include
+#pragma region Validacao Senha
+
+int ValidarSenha(char *senha_entry){
+int i;
+for (i = 1; i < MAX; i++){
+    if (!strcmp(senha_entry,usuario[i].senha))
+        return i;
+    if (!strcmp(senha_entry,usuario[i].senha))
+        return i;
+}
+if (i >= MAX)
+    return 0;
+}
+
+
+#pragma endregion
+
+#pragma region Funcao Incluir
 
 void IncluirUsuarios(int indiceUsuario,char *nome,long long int cpf,char *email,char *endereco,int numero,char *complemento,char *cidade_estado,long long int telefone,char *senha,int verifcadorUsuario){
     
@@ -155,13 +177,14 @@ void IncluirUsuarios(int indiceUsuario,char *nome,long long int cpf,char *email,
         cadastrar[indiceUsuario].telefone = telefone;
         strcpy(cadastrar[indiceUsuario].senha, senha);
         printf("Cliente cadastrado com sucesso!\n");
+        RegistrarLoginUsuario();
         indiceUsuario++;
     }    
 }
 
 #pragma endregion
 
-#pragma region Funcao Update
+#pragma region Funcao Atualizar
 
 void AtualizarDados(int id,char *nome,long long int cpf,char *email,char *endereco,int numero,char *complemento,char *cidade_estado,long long int telefone,char *senha){
     strcpy(cadastrar[id].nome, nome);
@@ -173,12 +196,12 @@ void AtualizarDados(int id,char *nome,long long int cpf,char *email,char *endere
     strcpy(cadastrar[id].cidade_estado, cidade_estado);
     cadastrar[id].telefone = telefone;
     strcpy(cadastrar[id].senha, senha);
-    printf("Atualizacao realizada com sucesso!");    
+    printf("Atualizacao realizada com sucesso!\n");    
 }
 
 #pragma endregion
 
-#pragma region Funcao Delete
+#pragma region Funcao Excluir
 
 void ExcluirCliente(int id){
     char nome[60];
@@ -213,32 +236,44 @@ void ExcluirCliente(int id){
 
 #pragma region Funcao Listar Cliente
 
-int ListClient(){
+void ListarClientes(){
+    ListarAdmin();
     for (int i = 1; i < MAX; i++)
-        if (!cadastrar[i].id == 0){
+        if (!cadastrar[i].id == 0)
             printf("Cliente: %d , %s , %lld , %s , %s , %d , %s ,%lld\n",cadastrar[i].id,
             cadastrar[i].nome,cadastrar[i].cpf,cadastrar[i].email,cadastrar[i].endereco,
-            cadastrar[i].numero,cadastrar[i].cidade_estado,cadastrar[i].telefone);
-        }else if (!cadastrarAdmin[i].id == 0)
-        {
+            cadastrar[i].numero,cadastrar[i].cidade_estado,cadastrar[i].telefone);        
+        
+}
+
+void ListarAdmin(){
+    for (int i = 1; i < MAX; i++)
+        if (!cadastrarAdmin[i].id == 0)
             printf("Admin: %d , %s , %lld , %s , %s , %d , %s ,%lld\n",cadastrarAdmin[i].id,
             cadastrarAdmin[i].nome,cadastrarAdmin[i].cpf,cadastrarAdmin[i].email,cadastrarAdmin[i].endereco,
             cadastrarAdmin[i].numero,cadastrarAdmin[i].cidade_estado,cadastrarAdmin[i].telefone);
-        }
-        
 }
 
 #pragma endregion
 
 #pragma region Banco de dados de clientes
 
-void InsertClientsDB(){
-    for (indiceUsuario = 1; indiceUsuario < 6; indiceUsuario++)
-        IncluirUsuarios(indiceUsuario,"teste",12312312312,"teste@teste.com","rua teste",indiceUsuario,"Casa","CWB-PR",41123451234,"senha",0);
+void InsereClientesBancoDeDados(){
+    // InsereAdmin();
+    // for (indiceUsuario = 1; indiceUsuario < 6; indiceUsuario++)
+        IncluirUsuarios(indiceUsuario,"teste",12312312312,"teste@teste.com","rua teste",indiceUsuario,"Casa","CWB-PR",41123451234,"senha",0);  
+        IncluirUsuarios(indiceUsuario,"teste1",12312312312,"teste1@teste.com","rua teste1",indiceUsuario,"Casa1","CWB-PRs",41123451234,"senha1",0);  
+        IncluirUsuarios(indiceUsuario,"teste2",12312312312,"teste2@teste.com","rua teste2",indiceUsuario,"Casa2","CWB-PRd",41123451234,"senha2",0);  
+        IncluirUsuarios(indiceUsuario,"teste3",12312312312,"teste3@teste.com","rua teste3",indiceUsuario,"Casa3","CWB-PRx",41123451234,"senha3",0); 
+        IncluirUsuarios(indiceUsuario,"testeAdmin",98798798712,"admin@teste.com","rua admin",indiceAdmin,"Sobrado","Matinhos-PR",41987651234,"senhaAdmin",1);
+        IncluirUsuarios(indiceUsuario,"testeAdmin1",98798798712,"admin1@teste.com","rua admin1",indiceAdmin,"Sobrado1","Matinhos-PRs",41987651234,"senhaAdmin1",1); 
+ 
+    RegistrarLoginAdmin();
+}
 
-    for (int ii = 6; ii < 9; ii++)
-        IncluirUsuarios(indiceUsuario,"testeAdmin",12312312312,"teste@teste.com","rua teste",indiceAdmin,"Casa","CWB-PR",41123451234,"senha",1);
-    
+void InsereAdmin(){
+    for (int indiceUsuario = 6; indiceUsuario < 9; indiceUsuario++)
+        IncluirUsuarios(indiceUsuario,"testeAdmin",98798798712,"admin@teste.com","rua admin",indiceAdmin,"Sobrado","Matinhos-PR",41987651234,"senhaAdmin",1);
 }
 
 #pragma endregion
@@ -279,7 +314,7 @@ int id = 0,numero = 0;
     }
 
     printf("Insira o Id para alterar:");
-    scanf("%d",id);    
+    scanf("%d",&id);    
     printf("--------------------------------------\n");
     printf("Nome: ");
     scanf(" %[^\n]s",&nome);
@@ -300,10 +335,10 @@ int id = 0,numero = 0;
     printf("Senha: ");
     scanf(" %[^\n]s", &senha);
 
-    printf("--------------------------------------\n");
-    printf("Alteracao realizada com sucesso: %d , %s , %d , %s , %s , %d , %s , %s , %d , %s\n POR:\n %s , %d , %s , %s , %d , %s , %s , %d , %s\n",id,cadastrar[id].nome,cadastrar[id].cpf,
-    cadastrar[id].email,cadastrar[id].complemento,cadastrar[id].cidade_estado,cadastrar[id].telefone,cadastrar[id].senha,nome,cpf,email,endereco,numero,complemento,cidade_uf,telefone,senha);
-    printf("--------------------------------------\n");
+    printf("---------------------------------------------------\n");
+    printf("DE: %d , %s , %lld , %s , %s , %d , %s , %s , %lld , %s\n",id,cadastrar[id].nome,cadastrar[id].cpf,cadastrar[id].email,cadastrar[id].endereco,cadastrar[id].numero,cadastrar[id].complemento,cadastrar[id].cidade_estado,cadastrar[id].telefone,cadastrar[id].senha); 
+    printf("POR: %s , %lld , %s , %s , %d , %s , %s , %lld , %s\n",nome,cpf,email,endereco,numero,complemento,cidade_uf,telefone,senha);
+    printf("---------------------------------------------------\n");
     opcao = 0;
     printf("Para cancelar tecle: 0\nPara confirmar tecle: 1\n");
     scanf("%d", &opcao);
@@ -315,7 +350,7 @@ int id = 0,numero = 0;
     }else
     {
         AtualizarDados(id,nome,cpf,email,endereco,numero,complemento,cidade_uf,telefone,senha);
-        CadastroLoginUsuario();
+        RegistrarLoginUsuario();
         printf("Atualizacao efetuada com sucesso!\n");
         main();
     }
@@ -357,7 +392,7 @@ int id = 0;
     }
 
     printf("Insira o Id para ser excluir:");
-    scanf("%d",id);    
+    scanf("%d",&id);    
     printf("--------------------------------------\n");
     ExcluirCliente(id);
     main();
@@ -365,27 +400,30 @@ int id = 0;
 
 #pragma endregion
 
-#pragma region CadastroLoginUser
+#pragma region Registrar Login Cliente
 
-void CadastroLoginUsuario(){  
-    strcpy(usuario[indiceCadastroUser].login,cadastrar[contador].email);
-    strcpy(usuario[indiceCadastroUser].senha,cadastrar[contador].senha);
-    indiceCadastroUser++;
+void RegistrarLoginUsuario(){  
+    strcpy(usuario[indiceCadastroUsuario].login,cadastrar[indiceUsuario].email);
+    strcpy(usuario[indiceCadastroUsuario].senha,cadastrar[indiceUsuario].senha);
+    indiceCadastroUsuario++;
 }
 
 #pragma endregion
 
-#pragma region CadastroLoginAdmin
+#pragma region Registrar Login Admin
 
-void cadastroLoginAdmin(){
-    strcpy(usuario[indiceCadastroUserAdmin].login,cadastrar[contadorAdmin].email);
-    strcpy(usuario[indiceCadastroUserAdmin].senha,cadastrar[contadorAdmin].senha);
-    indiceCadastroUserAdmin++;
+void RegistrarLoginAdmin(){
+    for (int i = 1; i < MAX; i++)
+        if (!cadastrarAdmin[i].id == 0){
+            strcpy(usuario[indiceCadastroAdmin].adminUser,cadastrarAdmin[indiceUsuario].email);
+            strcpy(usuario[indiceCadastroAdmin].senha,cadastrarAdmin[indiceUsuario].senha);
+            indiceCadastroAdmin++;
+        }
 }
 
 #pragma endregion
 
-#pragma region PaginaInicial
+#pragma region Pagina Inicial
 
 int dashboard()
 {
@@ -397,7 +435,7 @@ int dashboard()
 
 #pragma endregion
 
-#pragma region PaginaInicialAdmin
+#pragma region Pagina Inicial Admin
 
 int dashboardAdmin()
 {
@@ -408,7 +446,7 @@ int dashboardAdmin()
 
 #pragma endregion
 
-#pragma region CadastroCliente
+#pragma region Cadastro Cliente
 
 int cadastroCliente()
 {
@@ -478,7 +516,7 @@ int numero = 0;
     }else
     {
         IncluirUsuarios(indiceUsuario,nome,cpf,email,endereco,numero,complemento,cidade_uf,telefone,senha,0);
-        CadastroLoginUsuario();
+        RegistrarLoginUsuario();
         printf("Cadastro efetuado com sucesso!\n");
         main();
     }
@@ -487,7 +525,7 @@ int numero = 0;
 
 #pragma endregion
 
-#pragma region CadastroAdmin
+#pragma region Cadastro Admin
 
 int cadastroAdmin()
 {
@@ -556,7 +594,7 @@ int numero = 0,idFuncionario = 0;
     }else
     {
         IncluirUsuarios(indiceUsuario,nome,cpf,email,endereco,numero,complemento,cidade_uf,telefone,senha,1);
-        cadastroLoginAdmin();
+        RegistrarLoginAdmin();
         printf("Cadastro efetuado com sucesso!\n");
         main();
     }
@@ -595,11 +633,11 @@ int main()
       cadastroCliente();
       break;
     case 3:
-      InsertClientsDB();
+      InsereClientesBancoDeDados();
       main();
       break;
     case 4:
-      ListClient();
+      ListarClientes();
       main();
       break;
     case 5:
